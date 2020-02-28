3eb925861d9cdd3137c175713e11da9f06f73ff0
ext.atatarintsev <ext.atatarintsev@tcsbank.ru>
2020-02-28 17:42:22 +0300

    udf have been fixed

--- src/main/scala/booster/DataFrameApp.scala
+++ src/main/scala/booster/DataFrameApp.scala
@@ -1,6 +1,6 @@
 package booster
 
-import org.apache.spark.sql.expressions.Window
+import org.apache.spark.sql.expressions.{UserDefinedFunction, Window}
 import org.apache.spark.sql.functions.{col, row_number, udf}
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
@@ -8,6 +8,42 @@ import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
 import scala.util.Try
 
 object DataFrameApp {
+  def userVisitsCsv: StructType = {
+    StructType(Array(
+      StructField("ip", StringType, nullable = true),
+      StructField("url", StringType, nullable = true),
+      StructField("date", StringType, nullable = true),
+      StructField("revenue", FloatType, nullable = true),
+      StructField("ua", StringType, nullable = true),
+      StructField("country", StringType, nullable = true),
+      StructField("lang", StringType, nullable = true),
+      StructField("search", StringType, nullable = true),
+      StructField("duration", IntegerType, nullable = true)))
+  }
+  def blocksCsv: StructType = {
+    StructType(Array(
+      StructField("startIpNum", LongType, nullable = true),
+      StructField("endIpNum", LongType, nullable = true),
+      StructField("locId", IntegerType, nullable = true)))
+  }
+  def locationCsv: StructType = {
+    StructType(Array(
+      StructField("locId", IntegerType, nullable = true),
+      StructField("l_country", StringType, nullable = true),
+      StructField("l_region", StringType, nullable = true),
+      StructField("city", StringType, nullable = true),
+      StructField("l_postalCode", StringType, nullable = true),
+      StructField("l_latitude", FloatType, nullable = true),
+      StructField("l_longitude", FloatType, nullable = true),
+      StructField("l_metroCode", StringType, nullable = true),
+      StructField("l_areaCode", StringType, nullable = true)))
+  }
+  val sqlIpv4ToLong: UserDefinedFunction = udf((ip: String) =>
+    Try(ip.split('.').ensuring(_.length == 4)
+      .map(_.toLong).ensuring(_.forall(x => x >= 0 && x < 256))
+      .reverse.zip(List(0, 8, 16, 24)).map(xi => xi._1 << xi._2).sum)
+      .toOption)
+  val sqlDateToYear: UserDefinedFunction = udf((date: String) => date.take(4))
 
   def main(args: Array[String]) {
 
@@ -22,131 +58,76 @@ object DataFrameApp {
     val hc = spark.sparkContext.hadoopConfiguration
     hc.set("fs.s3a.aws.credentials.provider", "org.apache.hadoop.fs.s3a.AnonymousAWSCredentialsProvider")
 
-    //    io.File(outputPath).deleteRecursively()
-
-    def userVisitsCsv = {
-      StructType(Array(
-        StructField("ip", StringType, nullable = true),
-        StructField("url", StringType, nullable = true),
-        StructField("date", StringType, nullable = true),
-        StructField("revenue", FloatType, nullable = true),
-        StructField("ua", StringType, nullable = true),
-        StructField("country", StringType, nullable = true),
-        StructField("lang", StringType, nullable = true),
-        StructField("search", StringType, nullable = true),
-        StructField("duration", IntegerType, nullable = true)))
-    }
-
-    def blocksCsv = {
-      StructType(Array(
-        StructField("startIpNum", LongType, nullable = true),
-        StructField("endIpNum", LongType, nullable = true),
-        StructField("locId", IntegerType, nullable = true)))
-    }
-
-    def locationCsv = {
-      StructType(Array(
-        StructField("locId", IntegerType, nullable = true),
-        StructField("l_country", StringType, nullable = true),
-        StructField("l_region", StringType, nullable = true),
-        StructField("city", StringType, nullable = true),
-        StructField("l_postalCode", StringType, nullable = true),
-        StructField("l_latitude", FloatType, nullable = true),
-        StructField("l_longitude", FloatType, nullable = true),
-        StructField("l_metroCode", StringType, nullable = true),
-        StructField("l_areaCode", StringType, nullable = true)))
-    }
-
-    val coder: (String => Option[Long]) = ipv4ToLong
-    val sqlIpv4ToLong = udf(coder)
-
-    val yearTaker: (String => String) = getYear
-    val sqlDateToYear = udf(yearTaker)
-
-    val userVisits = spark.read
-      .format("csv")
+    val userVisits = spark.read.format("csv")
       .schema(userVisitsCsv)
       .option("header", value = true)
       .load(uvPath)
       .withColumn("ip", sqlIpv4ToLong(col("ip")))
 
-    val geoipBlocks = spark.read
-      .format("csv")
+    val geoipBlocks = spark.read.format("csv")
       .schema(blocksCsv)
       .option("header", value = true)
       .load(geoipBlocksPath)
 
-    val geoipLocation = spark.read
-      .format("csv")
+    val geoipLocation = spark.read.format("csv")
       .schema(locationCsv)
       .option("header", value = true)
       .load(geoipLocationPath)
 
     val locations = geoipBlocks.join(geoipLocation, "locId")
-    //      .filter(col("city").isNotNull)
 
-    val w = Window.partitionBy("year", "city")
+    val yearlyRevenueWindow = Window.partitionBy("year")
       .orderBy(col("sum(revenue)").desc)
-//    val w2 = Window
-//      .partitionBy("year", "country")
-//      .orderBy(col("sum(revenue)").desc)
-//
-//    val refined = userVisits
-//      .withColumn("date", sqlDateToYear(col("date")))
-//      .withColumnRenamed("date", "year")
-//      .withColumn("rn", row_number.over(w2)).where(col("rn") <= 3)
-//      .orderBy(col("year").desc, col("country"), col("rn"))
 
     val uvl: DataFrame = userVisits.as("uv")
       .join(locations.as("loc"), col("uv.ip").between(col("loc.startIpNum"), col("loc.endIpNum")))
       .withColumn("date", sqlDateToYear(col("date")))
       .withColumnRenamed("date", "year")
       .filter(col("city").isNotNull)
-      .filter(col("year") > 2012)
+      .withColumn("year", col("year").cast(IntegerType))
+      .filter(col("year") > 2010)
       .select("year", "country", "city", "revenue")
       .groupBy("year", "country", "city")
       .sum("revenue")
-      .withColumn("rn", row_number.over(w)).where(col("rn") <= 3)
-      .orderBy(col("sum(revenue)").desc, col("rn").desc)
+      .withColumn("rank", row_number.over(yearlyRevenueWindow)).where(col("rank") <= 3)
+      .orderBy(col("year"), col("rank"))
+      .withColumnRenamed("sum(revenue)", "sum(adRevenue)")
+      .withColumnRenamed("country", "countryCode")
 
-    //    val uvl2 = uvl.select("date", "country", "city", "sum(adRevenue)")
-    //      .orderBy("sum(adRevenue)")
+    //    uvl
+    //      .write
+    //      .mode(SaveMode.Overwrite)
+    //      .option("header", value = true)
+    //      .csv(outputPath + "/uvl")
 
-    val namedFrames = List(
-      //      (userVisits, "/userVisits"),
-      //      (geoipBlocks, "/geoipBlocks"),
-      //      (geoipLocation, "/geoipLocation"),
-      //      (locations, "/locations"),
-//      (refined, "/refined"),
-      (uvl, "/uvl")
-    )
+    val namedFrames = List((uvl, "/uvl"))
     printFrames(namedFrames.map(_._1))
     writeFrames(namedFrames, outputPath)
+
+    spark.read
+      .format("csv")
+      .option("header", value = true)
+      .load(outputPath + "/uvl")
+      .orderBy(col("year"), col("rank"))
+      .repartition(1)
+      .write
+      .mode(SaveMode.Overwrite)
+      .option("header", value = true)
+      .csv(outputPath + "/merged")
   }
 
-  def ipv4ToLong(ip: String): Option[Long] = Try(
-    ip.split('.').ensuring(_.length == 4)
-      .map(_.toLong).ensuring(_.forall(x => x >= 0 && x < 256))
-      .reverse.zip(List(0, 8, 16, 24)).map(xi => xi._1 << xi._2).sum
-  ).toOption
-
-  def getYear(date: String): String = {
-    date.take(4)
-  }
-
-  def writeFrames(dfTuplesList: List[(DataFrame, String)], outputPath: String): Unit = {
+  def writeFrames(dfTuplesList: List[(DataFrame, String)], outputPath: String): Unit =
     dfTuplesList.foreach { df =>
-      df._1.write
+      df._1
+        .write
         .mode(SaveMode.Overwrite)
         .option("header", value = true)
         .csv(outputPath + df._2)
     }
-  }
 
-  def printFrames(dfList: List[DataFrame]): Unit = {
+  def printFrames(dfList: List[DataFrame]): Unit =
     dfList.foreach { df =>
       df.show
       df.printSchema
     }
-  }
 }
